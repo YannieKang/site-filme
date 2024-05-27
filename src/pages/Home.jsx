8e940dc517a776d0780a87f41f5e8a367eabbca6
YannieKang <yannieyshin@gmail.com>
2024-05-27 12:58:22 -0300

    site-filme

---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import data from '../artigo.json'
+
+function Home() {
+  console.log(data)
+    return(
+      <>
+        <main>
+          <input type="text" name="buscar" id="busca" placeholder="Digite aqui sua busca"/>
+          <div className='grid grid-cols-3 gap-4'>
+           
+             {data.map(filme => (
+                  <div className='card bg-slate-400' key={filme.title}>
+                  <h1>{filme.title}</h1>
+                  <img src={filme.image} alt={filme.title} />
+                  <div className='tag'>
+                    {filme.tags.map(tag => (
+                        <span key={tag} className='text-purple-900'> {tag} </span>
+                    ))}
+                  </div>
+                  <div className='texto'>
+                    {filme.text.map(texto => (
+
+                        <p key={texto}> {texto}</p>
+                    ))}
+                  </div>
+                  </div>
+                
+                ))
+            }
+          </div>
+          
+        </main>
+      </>
+    );
+}
+
+export default Home
