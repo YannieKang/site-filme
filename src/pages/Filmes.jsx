0c688ee1d7bd3323bcbac1cc6d96769ffdf2615d
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-17 12:57:09 -0300

    SITE-FILME

--- src/pages/Filmes.jsx
+++ src/pages/Filmes.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 
 function Filmes() {
-
+ 
     const [filmes, setFilmes] = useState([])
 
     const apiKey= 'api_key=7c572a9f5b3ba776080330d23bb76e1e'
--- src/pages/PaginaFilme.jsx
+++ src/pages/PaginaFilme.jsx
@@ -1,7 +1,33 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 function PaginaFilmes(){
-    return(
-        <h1>Página do filme</h1>
-    )
+
+    const {id} = useParams();
+    const [filmes, setFilmes] = useState([])
+
+
+    const urlBase = 'https://api.themoviedb.org/3/movie/'
+    const apiKey= 'api_key=7c572a9f5b3ba776080330d23bb76e1e'
+    const urlImg = 'https://image.tmdb.org/t/p/w1280/'
+    
+    useEffect( () => {
+
+        fetch(`${urlBase}${id}?${apiKey}&language=pt-BR`)
+        .then(response => response.json())
+        .then(response => setFilmes(response))
+        .catch(error => console.log(error))
+    
+    },[])
+
+    return( 
+        <>
+        <img src={`${urlImg}${filmes.backdrop_path}`}/>
+        <h1> {filmes.title}</h1>
+        <p>{filmes.overview}</p>
+        </>
+
+    );
 }
 
 export default PaginaFilmes
